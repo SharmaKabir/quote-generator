78001318bde70aab864c1efa348745b51aa1a72e
SharmaKabir <sharmakabir204@gmail.com>
2024-03-24 18:28:07 +0530

    Added twitter integration and new quotes generation

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+
+
+
+const quoteContainer=document.getElementById('quote-container');
+const quoteText=document.getElementById('quote');
+const authorText=document.getElementById('author');
+const twitterBtn=document.getElementById('twitter');
+const newQuoteBtn=document.getElementById('new-quote');
+
 //Get Quotes from API
 let apiQuotes=[];// using let as we set in itnially as empty array
 
 function newQuote(){
             const quote=apiQuotes[Math.floor(Math.random() *apiQuotes.length)];
-            console.log(quote);
+            //const author = quote.author.split(',')[0].trim(); 
+
+            if (!quote.author){
+                authorText.textContent="Unknown";
+            }else{
+                authorText.textContent= quote.author.split(',')[0].trim(); 
+            }
+            
+
+            //for length
+            if(quote.text.length>120){
+                quoteText.classList.add('long-quote');
+            } else{
+                quoteText.classList.remove('long-quote');
+            }
+
+            
+            quoteText.textContent=(quote.text);
 }
 
 async function getQuotes(){
@@ -22,5 +48,26 @@ async function getQuotes(){
         alert(error)
     }
 }
+
+
+//tweet
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+
+
+}
+
+
+
+//event lsitners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+
+
+
+
+
 //On load
 getQuotes();
